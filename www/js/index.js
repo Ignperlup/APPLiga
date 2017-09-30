bef2151a26b4e4f387f88b2f6fa637f216d5d721
Ignperlup <pl.ignacio@gmail.com>
2017-09-30 21:16:04 +0200

    joeee

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,19 @@
 
-
-
-
 $(document).ready(main);
 
-window.plugins.PushbotsPlugin.initialize("59cfe60d9b823a1c788b4567", {"android":{"sender_id":"445542397792"}});
 
-alert("EXTRATERREXTRES"); 
 
 var contador = 1;
  
 function main () {
+
+
+	window.plugins.PushbotsPlugin.initialize("59cfe60d9b823a1c788b4567", {"android":{"sender_id":"445542397792"}});
+
+	alert("EXTRATERREXTRES"); 
+
+
+
 	$('.menu_bar').click(function(){
 		if (contador == 1) {
 			$('nav').animate({

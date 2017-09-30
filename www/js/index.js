9e8a40caea2d47e03ce0fd9cfe195c20e9efdd0e
Ignperlup <pl.ignacio@gmail.com>
2017-09-30 21:07:15 +0200

    not

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,13 @@
 
 
-window.plugins.PushbotsPlugin.initialize("59cfe60d9b823a1c788b4567", {"android":{"sender_id":"445542397792"}});
+
 
 $(document).ready(main);
- 
+
+window.plugins.PushbotsPlugin.initialize("59cfe60d9b823a1c788b4567", {"android":{"sender_id":"445542397792"}});
+
+alert("EXTRATERREXTRES"); 
+
 var contador = 1;
  
 function main () {
